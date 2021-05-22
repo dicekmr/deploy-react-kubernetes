e4a8149bc2ddb186b90393505b393f69f32807af
dicekmr <30400829+dicekmr@users.noreply.github.com>
2021-05-23 00:35:28 +0900

    Update App.js
    new2

--- src/containers/App.js
+++ src/containers/App.js
@@ -22,7 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Movie Finder</h1>
+        <h1>Movie Finder new2</h1>
         <InputSet />
         <List />
       </div>
